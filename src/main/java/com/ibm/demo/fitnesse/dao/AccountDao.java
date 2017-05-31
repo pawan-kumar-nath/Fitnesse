1a5a9c9b007aed88b38ec3a885b21e501d82db58
Pawan Kumar <pawar099@in.ibm.com>
2017-05-31 11:52:53 +0530

    updated table names

--- src/main/java/com/ibm/demo/fitnesse/dao/AccountDao.java
+++ src/main/java/com/ibm/demo/fitnesse/dao/AccountDao.java
@@ -5,7 +5,7 @@ import org.skife.jdbi.v2.sqlobject.SqlQuery;
 
 public interface AccountDao {
   
-   @SqlQuery("SELECT NVL(SUM(A.BALANCE),0) BALANCE FROM CUSTOMER C, ACCOUNT A WHERE C.CUSTOMER_NAME = :customerName AND C.CUSTOMER_ID = A.CUSTOMER_ID")
+   @SqlQuery("SELECT NVL(SUM(A.BALANCE),0) BALANCE FROM DEMO_CUSTOMER C, DEMO_ACCOUNT A WHERE C.CUSTOMER_NAME = :customerName AND C.CUSTOMER_ID = A.CUSTOMER_ID")
    int getBalanceByCustomerName(@Bind("customerName") String customerName);
    
 }
--- src/main/java/com/ibm/demo/fitnesse/dao/CustomerDao.java
+++ src/main/java/com/ibm/demo/fitnesse/dao/CustomerDao.java
@@ -6,12 +6,12 @@ import org.skife.jdbi.v2.sqlobject.SqlUpdate;
 
 public interface CustomerDao {
 
-   @SqlUpdate("INSERT INTO CUSTOMER (CUSTOMER_ID, CUSTOMER_NAME) VALUES (CUST_SEQ.NEXTVAL, :customerName)")
+   @SqlUpdate("INSERT INTO DEMO_CUSTOMER (CUSTOMER_ID, CUSTOMER_NAME) VALUES (DEMO_CUST_SEQ.NEXTVAL, :customerName)")
    int createCustomer(@Bind("customerName") String customerName);
 
-   @SqlQuery("SELECT CUSTOMER_ID FROM CUSTOMER WHERE TRIM(CUSTOMER_NAME) = TRIM(:customerName) ")
+   @SqlQuery("SELECT CUSTOMER_ID FROM DEMO_CUSTOMER WHERE TRIM(CUSTOMER_NAME) = TRIM(:customerName) ")
    int getCustomerId(@Bind("customerName") String customerName);
 
-   @SqlUpdate("DELETE FROM CUSTOMER WHERE TRIM(CUSTOMER_NAME) = TRIM(:customerName)")
+   @SqlUpdate("DELETE FROM DEMO_CUSTOMER WHERE TRIM(CUSTOMER_NAME) = TRIM(:customerName)")
    int deleteCustomer(@Bind("customerName") String customerName);
 }
